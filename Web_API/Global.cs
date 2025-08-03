1ef36139709123246932ef2a6c5b9136a2865e96
YoussefAhmedy <youssef.ahmedy189@gmail.com>
2025-08-03 15:03:31 +0300

    Create Global.cs

---
+++ Web_API/Global.cs
@@ -0,0 +1,15 @@
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace UserLogin
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
